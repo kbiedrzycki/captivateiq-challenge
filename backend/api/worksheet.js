971aa3d91cd6903509b0dd15dfa66941da283217
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2020-07-16 22:38:01 +0200

    Stubs for backend endpoints related to worksheets

---
+++ backend/api/worksheet.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports.list = async event => {
+  return event;
+};
+
+module.exports.get = async event => {
+  return event;
+};
+
+module.exports.create = async event => {
+  return event;
+};
+
+module.exports.update = async event => {
+  return event;
+};

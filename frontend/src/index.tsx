b828d4cf9b247d233b71c6bdc79367d76e099e50
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2020-07-16 22:25:46 +0200

    Introduce router to handle list and details pages separately

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import { Details } from './pages/worksheets/details';
+import { List } from './pages/worksheets/list';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Details />
+    <BrowserRouter>
+      <div className="container-fluid">
+        <Switch>
+          <Route path="/details/:id" component={Details} />
+          <Route path="/list" component={List} />
+          <Route path="*">
+            <Redirect to="/list" />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
---
+++ frontend/src/pages/worksheets/list.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const List = () => (
+  <div>test</div>
+);
